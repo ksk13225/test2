cff19a83826373c22909002145810623823ec92b
肇甫 高柳 <ksk13225@outlook.jp>
2020-04-11 10:59:38 +0900

    さようならを追加

--- Windows20200408/Windows20200408/Form1.vb
+++ Windows20200408/Windows20200408/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MessageBox.Show("こんにちは")
-        MessageBox.Show("ありがとう")
+        MessageBox.Show("さようなら")
+
     End Sub
 End Class
