135fe4b63a820c03e7e6c31ece889da1a39764cd
肇甫 高柳 <ksk13225@outlook.jp>
2020-04-11 10:54:30 +0900

    form1.vb messagebox追加

---
+++ Windows20200408/Windows20200408/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MessageBox.Show("こんにちは")
+        MessageBox.Show("ありがとう")
+    End Sub
+End Class
